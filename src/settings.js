3c2c266d062c5a46ca38cbdfb2af9ecd100d9a5e
qianduanxiaoliu <2461362522@qq.com>
2022-07-13 11:50:11 +0800

    '项目初始化'

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主体上面的header是否是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏logo是否显示
+   */
+  sidebarLogo: true
+}
